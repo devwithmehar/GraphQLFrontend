ed0a960ab1c1c0edf4ad29007236a27f491f3b4c
sahil937 <meharsahil207@gmail.com>
2022-11-23 16:41:50 -0600

    update: ADD_CLIENT mutation is written

--- src/components/AddClientModal.jsx
+++ src/components/AddClientModal.jsx
@@ -1,6 +1,10 @@
 import React , { useState } from 'react';
 import { FaUser } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
+import { ADD_CLIENT } from '../mutations/clientMutations';
+import { GET_CLIENTS } from '../queries/clientQueries';
+
+
 
 const AddClientModal = () => {
 
@@ -8,10 +12,36 @@ const AddClientModal = () => {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
 
+  const [addClient] = useMutation(ADD_CLIENT,{
+    variables: {
+      name,
+      email,
+      phone
+    },
+    update(cache, {
+      data: {addClient}
+    }){
+      const { clients } = cache.readQuery({query: GET_CLIENTS});
+
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: [...clients, addClient] },
+      });
+    }
+  });
+
 
   const onSubmit = (e) => {
     e.preventDefault();
-    
+    if(name === '' || email === '' || phone === ''){
+      return alert('All Fields are required');
+    }
+
+    addClient(name,email, phone);
+
+    setName('');
+    setEmail('');
+    setPhone('');
   }
 
 
