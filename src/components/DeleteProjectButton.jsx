a47bd3e540d4466d404db37fa8275966a199246e
sahil937 <meharsahil207@gmail.com>
2022-11-30 04:52:16 -0600

    update bugs are fixed

--- src/components/DeleteProjectButton.jsx
+++ src/components/DeleteProjectButton.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FaTrash } from 'react-icons/fa';
 import { GET_PROJECTS } from '../queries/projectQueries';
 import { useMutation } from '@apollo/client';
