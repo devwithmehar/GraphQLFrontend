c59c58bdc07361b202e13227022a2dcbfaed3692
sahil937 <meharsahil207@gmail.com>
2022-11-30 03:13:04 -0600

    Update: LINK from react-router-dom is used

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ProjectCard = ({ project }) => {
   return (
@@ -10,9 +11,9 @@ const ProjectCard = ({ project }) => {
               {project.name}
             </h5>
 
-            <a className='btn btn-light' href={`/projects/${project.id}`} >
+            <Link className='btn btn-light' to={`/project/${project.id}`} >
              View
-            </a>
+            </Link>
 
           </div>
 
